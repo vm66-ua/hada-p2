d516c5701f8d868b82ebe3ce31e68519b2458ec9
Vlada Mukhareva <vm66@mscloud.ua.es>
2025-03-02 20:45:54 +0100

    Clase eventos.cs hecha Y8521859M

---
+++ hada-p2/Eventos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Hada;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Hada
+{
+    public class TocadoArgs : EventArgs
+    {
+        private string _nombre;
+        private Coordenada _coordenadaImpacto;
+        public string Nombre => _nombre;
+
+        public Coordenada CoordenadaImpacto => _coordenadaImpacto;
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            _nombre = nombre;
+            _coordenadaImpacto = coordenadaImpacto;
+        }
+    }
+
+    public class HundidoArgs : EventArgs
+    {
+        public string _nombre;
+
+        public string Nombre => _nombre;
+        public HundidoArgs(string nombre)
+        {
+            _nombre = nombre;
+        }
+    }
+}
+
+
